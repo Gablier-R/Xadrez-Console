e61b628158a20ddc8890560394175f4fbc66c982
Gabriel Rodrigues P. Rocha <gabrielrodrigues1927@gmail.com>
2022-03-08 13:55:03 -0300

    Criando a classe Posição

--- Xadez-Console/Program.cs
+++ Xadez-Console/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Tabuleiro;
+
+namespace XadrezConsole
+{
+    class Program 
+    {
+        static void Main() 
+        {
+            Posicao p = new Posicao(3, 4);
+
+            Console.WriteLine(p);
+            Console.ReadLine();
+        }
+    }
+}
